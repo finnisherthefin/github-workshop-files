866716fd58219ea1fdcbfd50338a05a4ddcd1275
finnisherthefin <55222116+finnisherthefin@users.noreply.github.com>
2023-11-27 18:03:13 -0800

    jeff bezos really sucks so I have made these modifications to the code

--- random_number.py
+++ random_number.py
@@ -1,4 +1,4 @@
 import random
 
 # random.randint(a,b) generates a random number between a and b inclusive.
-print(f"The random number is {random.randint(0,9)}")
+print(f"The random number is really long! {random.randint(0,9)}")
