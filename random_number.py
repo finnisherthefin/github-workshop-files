5a5000b675920b25bccaf8423571c492530851e3
Carlo Aqui <carloaqui.8@gmail.com>
2023-11-27 18:05:21 -0800

    Carlo's Changes

--- random_number.py
+++ random_number.py
@@ -2,3 +2,8 @@ import random
 
 # random.randint(a,b) generates a random number between a and b inclusive.
 print(f"The random number is {random.randint(0,9)}")
+
+print("I program in C where is the f in print?")
+print("BIM BIM BAM BAM")
+# Why does this comment without //
+
